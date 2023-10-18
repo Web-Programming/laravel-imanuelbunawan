4f68dd1dfb1c3407832d0c6d380b3a42197ce17d
ImmanuelBunawan <bunawanimanuel@gmail.com>
2023-10-18 12:08:59 +0700

    update Controller

---
+++ app/Http/Controllers/KurikulumController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class KurikulumController extends Controller {
+
+        @return\Illuminate\Http\Response
+    public function index(){
+        
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProdiController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -65,3 +66,5 @@ Route::get('/fakultas' , function() {
     $fakultas = ["Fakultas Ilmu Komputer dan Rekayasa", "Fakultas Ilmu Ekonomi"];
     return view('fakultas.index', compact('fakultas', 'kampus'));
 });
+
+Route::get('/prodi',[ProdiController::class, 'index']);
